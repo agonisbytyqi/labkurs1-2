a2bc05dc8db183f8642cfa808daba98e7a453fb5
agonisbytyqi <bytyqiagonis@gmail.com>
2021-06-09 02:37:04 +0200

    backend of crud forms

---
+++ BACK/SQL/RoomReservation.sql
@@ -0,0 +1,63 @@
+create database TravelApp
+
+use TravelApp
+
+create table Room (
+RoomId int PRIMARY KEY identity(1,1),
+RoomName varchar(500),
+StatusOfRoom varchar(500),
+DateOfJoining date,
+DateOfExit date,
+ID INT FOREIGN KEY REFERENCES Reservation(ReservationId)
+)
+
+create table Room1 (
+RoomId int PRIMARY KEY identity(1,1),
+RoomName varchar(500),
+StatusOfRoom varchar(500),
+DateOfJoining date,
+DateOfExit date,
+Price int not null
+)
+
+
+use TravelApp
+select * from dbo.Room1
+insert into dbo.Room1 values ('VIP','FREE','2020-06-01','2020-06-05',150)
+
+
+create table Customer (
+CustomerId int PRIMARY KEY identity(1,1),
+FirstName varchar(500),
+LastName varchar(500),
+Phone varchar(500),
+Email varchar(500),
+City varchar(500),
+ID INT FOREIGN KEY REFERENCES Room(RoomId)
+)
+
+drop table Customer
+
+Create table Reservation (
+ReservationId int PRIMARY KEY identity(1,1),
+ReservationName varchar(500),
+)
+
+create table VendetTuristike (
+VendiId int PRIMARY KEY identity(1,1),
+VendiName varchar(500),
+VendLokacioni varchar(500),
+)
+
+select * from VendetTuristike
+insert into dbo.VendetTuristike values ('BOGE','RUGOVE')
+
+create table Gallery (
+PhotoId int identity(1,1),
+PhotoFile varchar(500),
+PhotoDescription varchar(500)
+)
+
+insert into Gallery values ('realmadrid.png','halamadrid')
+
+select * from Gallery
